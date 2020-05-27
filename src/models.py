6f3f1a2722973a5950c3a98e0b67aa3b11d592ce
giorgiozannini <giorgiozannini97@gmail.com>
2020-05-27 19:09:03 +0200

    resnet is working!

--- src/models.py
+++ src/models.py
@@ -92,7 +92,7 @@ class FullyCon_T4(nn.Module):
     Fully connected for the 4th task
     """
     
-    def __init__(self, task_train, w, h):
+    def __init__(self, device, task_train, w, h):
         super(FullyCon_T4, self).__init__()
 
         self.largest_w = w
@@ -134,11 +134,11 @@ class CNN_T4(nn.Module):
     CNN for the 4th task
     """
     
-    def __init__(self, task_train, sh1_big, sh2_big):
+    def __init__(self, task_train, w, h):
         super(CNN_T4, self).__init__()
 
-        self.largest_w = sh1_big
-        self.largest_h = sh2_big
+        self.largest_w = w
+        self.largest_h = h
         self.conv = nn.Conv2d(in_channels=10, out_channels=10, kernel_size=5, padding=2)
 
     def forward(self, x):  
@@ -146,5 +146,143 @@ class CNN_T4(nn.Module):
         x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
         x = x.unsqueeze(0)
         x = self.conv(x)
-        x = x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
+        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
+        return x
+
+class BasicBlock(nn.Module):
+    """
+    BasicBlock implementation for ResNet
+    
+    reference: https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+    """
+    
+    expansion = 1
+
+    def __init__(self, device, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.device = device
+        self.stride = stride
+        self.in_planes = in_planes
+        self.planes = planes
+        
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, 
+                              stride=stride, padding=2, bias=False)
+        # self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, bias=False)
+        # self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False)
+                # nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self,x):
+        out = F.relu(self.conv1(x))
+        out = self.conv2(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+    
+    def _forward(self, n, i, weights, x):
+        running_mu = torch.zeros(self.planes).to(self.device)
+        running_std = torch.ones(self.planes).to(self.device)
+        out = F.conv2d(x, weights["layer{}.{}.conv1.weight".format(n,i)], stride = self.stride,padding = 2)
+        out = F.relu(out)#F.batch_norm(out,running_mu, running_std,
+                      #  weights["layer{}.{}.bn1.weight".format(n,i)], 
+                      #  weights["layer{}.{}.bn1.bias".format(n,i)],training = True))
+        out = F.conv2d(out, weights["layer{}.{}.conv2.weight".format(n,i)])
+        # out = F.batch_norm(out,  running_mu, running_std,
+                      #  weights["layer{}.{}.bn2.weight".format(n,i)],
+                      #  weights["layer{}.{}.bn2.bias".format(n,i)], training = True)
+        conv = 0
+        if self.stride != 1 or self.in_planes != self.expansion*self.planes:
+            
+            conv = F.conv2d(x, weights["layer{}.{}.shortcut.0.weight".format(n,i)],stride = self.stride)
+            # bn = F.batch_norm(conv,  running_mu, running_std,
+                                      # weights["layer{}.{}.shortcut.1.weight".format(n,i)],
+                                      # weights["layer{}.{}.shortcut.1.bias".format(n,i)], training = True)
+          
+            x += conv
+                                
+        out = F.relu(out)
+        return out
+    
+    
+
+class ResNet(nn.Module):
+    """
+    ResNet implementation
+    
+    reference: https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+    """
+    
+    def __init__(self,device, task_train, w, h, block = BasicBlock, num_blocks = [1,1,1,1], num_classes=10):
+      
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.largest_w = w
+        self.largest_h = h
+        self.device = device
+        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
+
+        self.conv1 = nn.Conv2d(10, 64, kernel_size=3, padding = 1,stride=1, bias=False)
+        # self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(1, block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(2, block, 64, num_blocks[1], stride=1)
+        self.layer3 = self._make_layer(3, block, 64, num_blocks[2], stride=1)
+        self.layer4 = self._make_layer(4, block, 64, num_blocks[3], stride=1)
+        self.linear = nn.Linear(64*w*h, 10*self.out_dim[0]*self.out_dim[1])
+
+
+    def _make_layer(self, n, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.device, self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+   
+    def forward(self, x):
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
+        x = x.unsqueeze(0)
+        
+        out = F.relu(self.conv1(x))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        
+        x = torch.reshape(out, (1, 10, self.out_dim[0], self.out_dim[1])) 
+        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
+        return x
+     
+    def _forward(self, x, weights):
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
+        x = x.unsqueeze(0)
+        
+        out = F.conv2d(x, weights["conv1.weight"], padding = 1)
+        out = F.relu(out)#F.batch_norm(out,running_mu, running_std, training = True))
+        
+        for i, layer in enumerate(self.layer1):
+            out = layer._forward(1, i, weights, out)
+        for i, layer in enumerate(self.layer2):
+            out = layer._forward(2, i, weights, out)
+        for i, layer in enumerate(self.layer3):
+            out = layer._forward(3, i, weights, out)
+        for i, layer in enumerate(self.layer4):
+            out = layer._forward(4, i, weights, out)
+
+        out = out.view(out.size(0), -1)
+        out = F.linear(out, weights["linear.weight"], weights["linear.bias"])
+        
+        x = torch.reshape(out, (1, 10, self.out_dim[0], self.out_dim[1])) 
+        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
         return x
