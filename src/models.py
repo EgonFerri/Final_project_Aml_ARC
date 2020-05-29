096719b4d9792279c7698d62d23ddca5795d9096
scikibu <44502455+scikibu@users.noreply.github.com>
2020-05-29 19:36:19 +0200

    cnns for task 7

--- src/models.py
+++ src/models.py
@@ -181,15 +181,40 @@ class CNN_5(nn.Module):
             x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
         return x
     
-class CNN_10(nn.Module):
+class CNN_5_T7(nn.Module):
     """
-    CNN
+    CNN for the 7th task
     """
     
     def __init__(self, task_train, w, h, attention):
-        super(CNN_10 , self).__init__()
+        super(CNN_5_T7, self).__init__()
 
-        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
+        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
+        self.conv = nn.Conv2d(in_channels=10, out_channels=100, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=100, out_channels=10, kernel_size=5, padding=2)
+        self.attention = Attention(10, 10)
+        self.attention_value = attention
+
+    def forward(self, x):  
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = x.unsqueeze(0)
+        x = pad_crop(x, self.out_dim[0], self.out_dim[1], input_w, input_h, goal='crop')        
+        x = self.conv(x)
+        x =self.conv2(x)
+        if self.attention_value is not None:
+            x = torch.reshape(x, (1, x.shape[2], x.shape[3], 10))
+            x = self.attention(x)
+            x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
+        return x
+    
+class CNN_10_T7(nn.Module):
+    """
+    CNN for the 7th task
+    """
+    
+    def __init__(self, task_train, w, h, attention):
+        super(CNN_10_T7 , self).__init__()
+
         self.out_dim = np.array(np.array(task_train[0]['output']).shape)
         self.conv = nn.Conv2d(in_channels=10, out_channels=100, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=100, out_channels=10, kernel_size=3, padding=1)
@@ -198,9 +223,8 @@ class CNN_10(nn.Module):
 
     def forward(self, x):  
         ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
-        x = pad_crop(x, self.out_dim[0], self.out_dim[1], self.inp_dim[0], self.inp_dim[1], goal='pad')
         x = x.unsqueeze(0)
-        #x = torch.nn.Upsample(size=(self.out_dim[0], self.out_dim[1]))(x)
+        x = pad_crop(x, self.out_dim[0], self.out_dim[1], input_w, input_h, goal='crop')
         x = self.conv(x)
         x =self.conv2(x)
         if self.attention_value is not None:
