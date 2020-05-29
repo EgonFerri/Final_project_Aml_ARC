ac32b28baeaa50b2ea97aadc3f9d276ec47aa268
EgonFerri <egon.ferri@gmail.com>
2020-05-29 15:37:26 +0200

    cose

--- src/models.py
+++ src/models.py
@@ -4,6 +4,150 @@ import torch.nn.functional as F
 import numpy as np
 from utils import pad_crop
 
+#----------------------------------_____________________ATTENTION___________________-----------------------------------  
+
+class Attention(nn.Module):
+    def __init__(self, input_dim, attention_dim):
+        super(Attention, self).__init__()
+        self.linear = nn.Linear(input_dim, attention_dim)
+        self.full_att = nn.Linear(attention_dim, 1)
+
+    def forward(self, x):
+        att = self.linear(x)
+        att = self.full_att(F.relu(x.unsqueeze(1))).squeeze(2) 
+        alpha = F.softmax(att)
+        out = (x * alpha.unsqueeze(2)).sum(dim=1)
+        return x
+    
+
+#----------------------------------_____________________DENSE___________________----------------------------------- 
+
+class FullyCon(nn.Module):
+    """
+    Fully connected
+    """
+  
+    def __init__(self, task_train, w, h, attention):
+        super(FullyCon, self).__init__()
+
+        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
+        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
+        self.linear = nn.Linear(self.inp_dim[0]*self.inp_dim[1]*10, self.out_dim[0]*self.out_dim[1]*10)
+
+    def forward(self, x):
+        x = x.view(-1) 
+        x = self.linear(x) 
+        x = torch.reshape(x, (1, 10, self.out_dim[0], self.out_dim[1]))
+        return x
+    
+class FullyCon_deep(nn.Module):
+    """
+    Fully connected
+    """
+  
+    def __init__(self, task_train, w, h, attention):
+        super(FullyCon_deep, self).__init__()
+
+        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
+        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
+        self.linear = nn.Linear(self.inp_dim[0]*self.inp_dim[1]*10, self.out_dim[0]*self.out_dim[1]*10)
+        self.linear2 = nn.Linear(self.out_dim[0]*self.out_dim[1]*10, self.out_dim[0]*self.out_dim[1]*10)
+        
+    def forward(self, x):
+        x = x.view(-1) 
+        x = self.linear(x)
+        x = F.leaky_relu(x)
+        x = self.linear2(x)
+        x = torch.reshape(x, (1, 10, self.out_dim[0], self.out_dim[1]))
+        return x
+    
+    
+    
+class FullyCon_T4(nn.Module):
+    """
+    Fully connected for the 4th task
+    """
+    
+    def __init__(self, task_train, w, h, attention):
+        super(FullyCon_T4, self).__init__()
+
+        self.largest_w = w
+        self.largest_h = h
+        self.linear = nn.Linear(self.largest_w*self.largest_h*10, self.largest_w*self.largest_h*10)
+
+    def forward(self, x):  
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad') 
+        x = x.view(-1).unsqueeze(0) 
+        x = self.linear(x) 
+        x = torch.reshape(x, (1, 10, self.largest_w, self.largest_h)) 
+        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
+        return x
+    
+#----------------------------------_____________________CNN___________________----------------------------------- 
+    
+    
+class CNN_5(nn.Module):
+    """
+    CNN
+    """
+    
+    def __init__(self, task_train, w, h, attention):
+        super(CNN_5, self).__init__()
+
+        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
+        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
+        self.conv = nn.Conv2d(in_channels=10, out_channels=100, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=100, out_channels=10, kernel_size=5, padding=2)
+        self.attention = Attention(10, 10)
+        self.attention_value = attention
+
+    def forward(self, x):  
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = pad_crop(x, self.out_dim[0], self.out_dim[1], self.inp_dim[0], self.inp_dim[1], goal='pad')
+        x = x.unsqueeze(0)
+        #x = torch.nn.Upsample(size=(self.out_dim[0], self.out_dim[1]))(x)
+        x = self.conv(x)
+        x =self.conv2(x)
+        if self.attention_value is not None:
+            x = torch.reshape(x, (1, x.shape[2], x.shape[3], 10))
+            x = self.attention(x)
+            x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
+        return x
+    
+class CNN_10(nn.Module):
+    """
+    CNN
+    """
+    
+    def __init__(self, task_train, w, h, attention):
+        super(CNN_10 , self).__init__()
+
+        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
+        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
+        self.conv = nn.Conv2d(in_channels=10, out_channels=100, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=100, out_channels=10, kernel_size=3, padding=1)
+        self.attention = Attention(10, 10)
+        self.attention_value = attention
+
+    def forward(self, x):  
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = pad_crop(x, self.out_dim[0], self.out_dim[1], self.inp_dim[0], self.inp_dim[1], goal='pad')
+        x = x.unsqueeze(0)
+        #x = torch.nn.Upsample(size=(self.out_dim[0], self.out_dim[1]))(x)
+        x = self.conv(x)
+        x =self.conv2(x)
+        if self.attention_value is not None:
+            x = torch.reshape(x, (1, x.shape[2], x.shape[3], 10))
+            x = self.attention(x)
+            x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
+        return x
+    
+    
+
+#----------------------------------_____________________LSTM___________________----------------------------------- 
+
+
 class LSTM(nn.Module):
     """
     LSTM
@@ -23,84 +167,92 @@ class LSTM(nn.Module):
         x = x.view(sh1, -1).unsqueeze(0)
         x, _ = self.lstm(x)
         if self.attention_value is not None:
-          x = torch.reshape(x, (1, x.shape[2], 10))
-          x = self.attention(x)
+            x = torch.reshape(x, (1, x.shape[2], 10))
+            x = self.attention(x)
         x = torch.reshape(x, (1, 10, self.out_dim[0], self.out_dim[1])) 
         return x
-    
 
-class CNN(nn.Module):
+class LSTM_T4(nn.Module):
     """
-    CNN
+    LSTM for the 4th task
+    """
+    def __init__(self, task_train, w, h, attention):
+        super(LSTM_T4, self).__init__() 
+
+        self.largest_w = w
+        self.largest_h = h
+        self.lstm = nn.LSTM(self.largest_w*self.largest_h, self.largest_w*self.largest_h, dropout=0, batch_first=True)
+        self.attention = Attention(10, 10)
+        self.attention_value = attention
+        
+    def forward(self, x):     
+        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
+        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
+        x = x.view(ch, -1).unsqueeze(0)
+        x, _ = self.lstm(x)
+        if self.attention_value is not None:
+            x = torch.reshape(x, (1, x.shape[2], 10))
+            x = self.attention(x)
+        x = torch.reshape(x, (1, 10, self.largest_w, self.largest_h)) 
+        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
+        return x
+
+    
+    
+#----------------------------------_____________________LSTM___________________----------------------------------- 
+
+
+class GRU(nn.Module):
+    """
+    GRU
     """
     
     def __init__(self, task_train, w, h, attention):
-        super(CNN, self).__init__()
+        super(GRU, self).__init__()
 
         self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
         self.out_dim = np.array(np.array(task_train[0]['output']).shape)
-        self.conv = nn.Conv2d(in_channels=10, out_channels=10, kernel_size=5, padding=2)
+        self.gru = nn.GRU(self.inp_dim[0]*self.inp_dim[1], self.out_dim[0]*self.out_dim[1], dropout=0, batch_first=True)
         self.attention = Attention(10, 10)
         self.attention_value = attention
 
-    def forward(self, x):  
-        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
-        x = pad_crop(x, self.out_dim[0], self.out_dim[1], self.inp_dim[0], self.inp_dim[1], goal='pad')
-        x = x.unsqueeze(0)
-        #x = torch.nn.Upsample(size=(self.out_dim[0], self.out_dim[1]))(x)
-        x = self.conv(x)
+    def forward(self, x): 
+        sh1, sh2, sh3 = x.shape[0], x.shape[1], x.shape[2]
+        x = x.view(sh1, -1).unsqueeze(0)
+        x, _ = self.gru(x)
         if self.attention_value is not None:
-          x = torch.reshape(x, (1, x.shape[2], x.shape[3], 10))
-          x = self.attention(x)
-          x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
+            x = torch.reshape(x, (1, x.shape[2], 10))
+            x = self.attention(x)
+        x = torch.reshape(x, (1, 10, self.out_dim[0], self.out_dim[1])) 
         return x
-    
-    
-class CNN_big(nn.Module):
-    """
-    CNN
-    """
-    
-    def __init__(self, task_train, w, h, attention):
-        super(CNN_big, self).__init__()
 
-        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
-        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
-        self.conv = nn.Conv2d(in_channels=10, out_channels=100, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(in_channels=100, out_channels=10, kernel_size=5, padding=2)
+class LSTM_T4(nn.Module):
+    """
+    LSTM for the 4th task
+    """
+    def __init__(self, task_train, w, h, attention):
+        super(LSTM_T4, self).__init__() 
+
+        self.largest_w = w
+        self.largest_h = h
+        self.lstm = nn.LSTM(self.largest_w*self.largest_h, self.largest_w*self.largest_h, dropout=0, batch_first=True)
         self.attention = Attention(10, 10)
         self.attention_value = attention
-
-    def forward(self, x):  
+        
+    def forward(self, x):     
         ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
-        x = pad_crop(x, self.out_dim[0], self.out_dim[1], self.inp_dim[0], self.inp_dim[1], goal='pad')
-        x = x.unsqueeze(0)
-        #x = torch.nn.Upsample(size=(self.out_dim[0], self.out_dim[1]))(x)
-        x = self.conv(x)
-        x =self.conv2(x)
+        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
+        x = x.view(ch, -1).unsqueeze(0)
+        x, _ = self.lstm(x)
         if self.attention_value is not None:
-          x = torch.reshape(x, (1, x.shape[2], x.shape[3], 10))
-          x = self.attention(x)
-          x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
+            x = torch.reshape(x, (1, x.shape[2], 10))
+            x = self.attention(x)
+        x = torch.reshape(x, (1, 10, self.largest_w, self.largest_h)) 
+        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
         return x
+
     
-class FullyCon(nn.Module):
-    """
-    Fully connected
-    """
-  
-    def __init__(self, task_train, w, h, attention):
-        super(FullyCon, self).__init__()
-
-        self.inp_dim = np.array(np.array(task_train[0]['input']).shape)
-        self.out_dim = np.array(np.array(task_train[0]['output']).shape)
-        self.linear = nn.Linear(self.inp_dim[0]*self.inp_dim[1]*10, self.out_dim[0]*self.out_dim[1]*10)
-
-    def forward(self, x):
-        x = x.view(-1) 
-        x = self.linear(x) 
-        x = torch.reshape(x, (1, 10, self.out_dim[0], self.out_dim[1]))
-        return x
+#----------------------------------_____________________META LEARNING___________________-----------------------------------    
     
 class MetaFullyCon(torch.nn.Module):
     """
@@ -128,94 +280,7 @@ class MetaFullyCon(torch.nn.Module):
         x = torch.flatten(x)
         x = F.linear(x, weights['lin.weight'], bias = weights['lin.bias'])
         x = x.reshape((1,10,self.out_dim[0],self.out_dim[1]))
-        return x
-    
-class FullyCon_T4(nn.Module):
-    """
-    Fully connected for the 4th task
-    """
-    
-    def __init__(self, task_train, w, h, attention):
-        super(FullyCon_T4, self).__init__()
-
-        self.largest_w = w
-        self.largest_h = h
-        self.linear = nn.Linear(self.largest_w*self.largest_h*10, self.largest_w*self.largest_h*10)
-
-    def forward(self, x):  
-        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
-        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad') 
-        x = x.view(-1).unsqueeze(0) 
-        x = self.linear(x) 
-        x = torch.reshape(x, (1, 10, self.largest_w, self.largest_h)) 
-        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
-        return x
-    
-  
-class LSTM_T4(nn.Module):
-    """
-    LSTM for the 4th task
-    """
-
-    def __init__(self, task_train, w, h, attention):
-        super(LSTM_T4, self).__init__() 
-
-        self.largest_w = w
-        self.largest_h = h
-        self.lstm = nn.LSTM(self.largest_w*self.largest_h, self.largest_w*self.largest_h, dropout=0, batch_first=True)
-        self.attention = Attention(10, 10)
-        self.attention_value = attention
-        
-    def forward(self, x):     
-        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
-        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
-        x = x.view(ch, -1).unsqueeze(0)
-        x, _ = self.lstm(x)
-        if self.attention_value is not None:
-          x = torch.reshape(x, (1, x.shape[2], 10))
-          x = self.attention(x)
-        x = torch.reshape(x, (1, 10, self.largest_w, self.largest_h)) 
-        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
-        return x
-    
-class CNN_T4(nn.Module):
-    """
-    CNN for the 4th task
-    """
-    
-    def __init__(self, task_train, w, h):
-        super(CNN_T4, self).__init__()
-
-        self.largest_w = w
-        self.largest_h = h
-        self.conv = nn.Conv2d(in_channels=10, out_channels=10, kernel_size=5, padding=2)
-        self.attention = Attention(10, 10)
-        self.attention_value = attention
-
-    def forward(self, x):  
-        ch, input_w, input_h = x.shape[0], x.shape[1], x.shape[2]
-        x = pad_crop(x, self.largest_w, self.largest_h, input_w, input_h, goal='pad')
-        x = x.unsqueeze(0)
-        x = self.conv(x)
-        if self.attention_value is not None:
-          x = torch.reshape(x, (1, x.shape[2], x.shape[3], 10))
-          x = self.attention(x)
-          x = torch.reshape(x, (1, 10, x.shape[1], x.shape[2]))
-        x = pad_crop(x, input_w, input_h, self.largest_w, self.largest_h, goal='crop')
-        return x
-    
-class Attention(nn.Module):
-  def __init__(self, input_dim, attention_dim):
-    super(Attention, self).__init__()
-    self.linear = nn.Linear(input_dim, attention_dim)
-    self.full_att = nn.Linear(attention_dim, 1)
-
-  def forward(self, x):
-    att = self.linear(x)
-    att = self.full_att(F.relu(x.unsqueeze(1))).squeeze(2) 
-    alpha = F.softmax(att)
-    out = (x * alpha.unsqueeze(2)).sum(dim=1)
-    return x
+        return x    
 
 class BasicBlock(nn.Module):
     """
@@ -279,7 +344,7 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
     
-    
+ 
 
 class ResNet(nn.Module):
     """
